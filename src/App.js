f04511b3c442d9cd496ae7da69c5517d25b2ae0e
Abhiram <ram1289@outlook.com>
2020-05-31 17:24:50 +0530

    code from the starter-react-contacts

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-import React, { Component } from 'react'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-
-class ContactList extends Component {
+class App extends Component {
   render() {
-    const people = this.props.contacts;
-
     return (
       <div>
-        <ol>
-          {people.map(person => (
-            <li key={person.name}>{person.name}</li>
-          ))}
-        </ol>
+        Hello World
       </div>
-    )
+    );
   }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ContactList
-        contacts={[{ name: 'James' }, { name: 'Mark' }, { name: 'Steven' }]}
-      />
-      <ContactList
-        contacts={[{ name: 'Evi' }, { name: 'Sarah' }, { name: 'Susan' }]}
-      />
-      <ContactList
-        contacts={[{ name: 'Spot' }, { name: 'Rover' }, { name: 'Fido' }]}
-      />
-      </header>
-      
-    </div>
-  );
 }
 
 export default App;
