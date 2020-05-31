e201d1d95c25bdf67ad5d4b87008816a62b6bdc3
Abhiram <ram1289@outlook.com>
2020-05-31 17:14:28 +0530

    test changes for App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+
+
+class ContactList extends Component {
+  render() {
+    const people = this.props.contacts;
+
+    return (
+      <div>
+        <ol>
+          {people.map(person => (
+            <li key={person.name}>{person.name}</li>
+          ))}
+        </ol>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ContactList
+        contacts={[{ name: 'James' }, { name: 'Mark' }, { name: 'Steven' }]}
+      />
+      <ContactList
+        contacts={[{ name: 'Evi' }, { name: 'Sarah' }, { name: 'Susan' }]}
+      />
+      <ContactList
+        contacts={[{ name: 'Spot' }, { name: 'Rover' }, { name: 'Fido' }]}
+      />
       </header>
+      
     </div>
   );
 }
